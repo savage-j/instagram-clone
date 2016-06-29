48f0c267dfb2f446caac03891a10186bd5ed4ce6
Jordan <savage@savagej.com>
2016-06-28 20:53:51 -0700

    user initialization test passing

---
+++ instagram-cloneTests/UserTests.m
@@ -0,0 +1,43 @@
+//
+//  UserTests.m
+//  instagram-clone
+//
+//  Created by Jordan Carlson on 6/28/16.
+//  Copyright © 2016 savagej. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "User.h"
+
+@interface UserTests : XCTestCase
+
+@end
+
+@implementation UserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testThatInitializationWorks
+{
+    NSDictionary *sourceDictionary = @{@"id": @"8675309",
+                                       @"username" : @"d'oh",
+                                       @"full_name" : @"Homer Simpson",
+                                       @"profile_picture" : @"http://www.example.com/example.jpg"};
+    User *testUser = [[User alloc] initWithDictionary:sourceDictionary];
+    
+    XCTAssertEqualObjects(testUser.idNumber, sourceDictionary[@"id"], @"The ID number should be equal");
+    XCTAssertEqualObjects(testUser.userName, sourceDictionary[@"username"], @"The username should be equal");
+    XCTAssertEqualObjects(testUser.fullName, sourceDictionary[@"full_name"], @"The full name should be equal");
+    XCTAssertEqualObjects(testUser.profilePictureURL, [NSURL URLWithString:sourceDictionary[@"profile_picture"]], @"The profile picture should be equal");
+}
+
+
+@end
